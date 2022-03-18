3c65598bf4bdd3c738d0d1c6835171ef81ee784b
Ernestekas <ernestekas@gmail.com>
2022-03-18 20:13:24 +0200

    Program file reconstructing finished.
    - Console logic moved to console service.
    - Added dependency injection.

---
+++ csharp/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using csharp.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace csharp
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddTransient<ConsoleService>();
+        }
+    }
+}
---
+++ csharp/Services/ConsoleService.cs
@@ -0,0 +1,33 @@
+﻿using csharp.Models;
+using System;
+
+namespace csharp.Services
+{
+    public class ConsoleService
+    {
+        public void Run()
+        {
+            Console.WriteLine("OMGHAI!");
+
+            Inventory inventory = new Inventory();
+            inventory.LoadDefaultInventory();
+
+            var app = new GildedRose(inventory.Items);
+
+
+            for (var i = 0; i < 31; i++)
+            {
+                Console.WriteLine("-------- day " + i + " --------");
+                Console.WriteLine("name, sellIn, quality");
+                for (var j = 0; j < inventory.Items.Count; j++)
+                {
+                    Console.WriteLine(inventory.Items[j]);
+                }
+                Console.WriteLine("");
+                app.UpdateQuality();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
